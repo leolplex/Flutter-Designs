669149e29a2e2a61e6e3d072e48e2f8f619720b3
Daniel <raiga1234@gmail.com>
2021-01-27 15:30:44 -0500

    Build first Page

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -14,10 +14,60 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _page1() {
-    return Center(child: Text('Page 1'),);
+    return Stack(
+      children: [_backgroundColor(), _backgoundImage(), _texts()],
+    );
   }
 
   Widget _page2() {
-    return Center(child: Text('Page 2'),);
+    return Center(
+      child: Text('Page 2'),
+    );
+  }
+
+  Widget _backgroundColor() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _backgoundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _texts() {
+    final textStyle = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            '11°',
+            style: textStyle,
+          ),
+          Text(
+            'Miercoles',
+            style: textStyle,
+          ),
+          Expanded(child: Container()),
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 70.0,
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
   }
 }
